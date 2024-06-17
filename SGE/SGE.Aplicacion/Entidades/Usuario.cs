3661513b7af91f1f8285203956a46beaab51aac7
j115 <elprimodecocossily3@gmail.com>
2024-06-17 18:08:58 -0300

    falta

--- SGE/SGE.Aplicacion/Entidades/Usuario.cs
+++ SGE/SGE.Aplicacion/Entidades/Usuario.cs
@@ -9,13 +9,20 @@ namespace SGE.Aplicacion.Entidades
 {
     public class Usuario
     {
+        public Usuario(string nombre, string apellido, string correo, string contrasenia)
+        {
+            Nombre = nombre;
+            Apellido = apellido;
+            Correo = correo; 
+            Contrasenia = contrasenia;
+        }
         
         public int Id { get; set; }
         public string Nombre { get; set; }
         public string Apellido { get; set;}
         public string Correo { get; set; }
         public string Contrasenia { get; set; }
-        public List<Permiso> Permisos { get; set; }
+        public List<Permiso> Permisos { get; set; } = new List<Permiso>();
 
     }
 }
--- SGE/SGE.Repositorio/SistemaContext.cs
+++ SGE/SGE.Repositorio/SistemaContext.cs
@@ -10,14 +10,23 @@ namespace SGE.Repositorio
 {
     public class SistemaContext : DbContext
     {
+        #nullable disable //por las dudas versiones anteriores (diapositiva 63 teoria 10)
         public DbSet<Usuario> Usuarios { get; set; }
         public DbSet<Expediente> Expedientes { get; set; }
         public DbSet<Tramite> Tramites { get; set; }
+        #nullable restore
 
-        protected override void OnConfiguring(DbContextOptionsBuilder
-            optionsBuilder)
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite("data source=Sistema.sqlite");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Tramite>()
+                .HasOne(t => t.Expediente)        // Tramite tiene un Expediente
+                .WithMany(e => e.listaTramites)   // Expediente tiene muchos Tramites
+                .HasForeignKey(t => t.IdExpediente); // Usar IdExpediente como clave foránea
         }
     }
 }
