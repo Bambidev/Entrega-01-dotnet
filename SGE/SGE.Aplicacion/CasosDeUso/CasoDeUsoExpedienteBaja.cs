eade863d4a59d665c224bcdaf08ccd41cc152ff2
j115 <elprimodecocossily3@gmail.com>
2024-06-19 19:50:20 -0300

    excepciones

--- SGE/SGE.Aplicacion/CasosDeUso/CasoDeUsoExpedienteBaja.cs
+++ SGE/SGE.Aplicacion/CasosDeUso/CasoDeUsoExpedienteBaja.cs
@@ -5,7 +5,7 @@ namespace SGE.Aplicacion.CasosDeUso
 {
     public class CasoDeUsoExpedienteBaja(IExpedienteRepositorio repoExpediente, ITramiteRepositorio repoTramite) //AL ELIMINAR UN EXPEDIENTE SE DEBEN ELIMINAR TODOS LOS TRAMITES RELACIONADOS AL MISMO
     {
-        public void Ejecutar(int idExp, int idEjecutor)
+        public void Ejecutar(int idExp)
         {
             string resultado = "";
             repoTramite.EliminarTramitesByExpediente(idExp);
